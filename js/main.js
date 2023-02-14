5dd98195bdd5386600a5a36e8333eb705f771bac
Douglas Silva <edmaker@gmail.com>
2023-02-14 15:08:11 -0300

    finalizado robotron

--- js/main.js
+++ js/main.js
@@ -1,20 +1,69 @@
-const subtrair = document.querySelector('#subtrair')
-const somar = document.querySelector('#somar')
-const braco = document.querySelector('#braco')
+const controle = document.querySelectorAll("[data-controle]") // todos os somar e subtrair
+const estatisticas = document.querySelectorAll("[data-estatistica]") // todos os somar e subtrair
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
 
-const controle = document.querySelectorAll(".controle-ajuste")
-console.log(controle)
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
+
 
 controle.forEach( (elemento) => {
     elemento.addEventListener("click", (evento) => {
-        manipulaDados(evento.target.textContent)
+        //dataset.controle => data-controle
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode) //parentNode passa qual parent esta sendo clicado
+        atualizaEstatisticas(evento.target.dataset.peca, evento.target.dataset.controle)
     })
 })
 
-function manipulaDados(operacao) {
+function manipulaDados(operacao, controle) {
+    const peca = controle.querySelector('[data-contador]') // peca recebe o parent clicado
+
+
+    //decide se foi subtrair ou somar
     if (operacao === '-') {
-        braco.value = parseInt(braco.value) - 1
+        peca.value = parseInt(peca.value) - 1 
     }else {
-        braco.value = parseInt(braco.value) + 1
+        peca.value = parseInt(peca.value) + 1
     }
+}
+
+function atualizaEstatisticas(peca, operacao) {
+    console.log(pecas[peca]) //puxa a estaticas da peca clicada
+    estatisticas.forEach( (elemento) => {
+        console.log(elemento.dataset.estatistica)
+
+        if (operacao === '-'){
+            elemento.textContent = parseInt(elemento.textContent) - pecas[peca][elemento.dataset.estatistica]
+        }else{
+            elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]
+        }
+    });
 }
